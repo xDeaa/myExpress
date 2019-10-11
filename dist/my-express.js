1317475c94ab0e4e3d33c83a9d717e0ef452cb99
xDeaa <andrea_christina@hotmail.fr>
2019-10-11 11:55:53 +0200

    init project

---
+++ dist/my-express.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = () => console.log("ok");
+//# sourceMappingURL=my-express.js.map
---
+++ lib/app.ts
@@ -0,0 +1,2 @@
+import myExpress from './my-express';
+myExpress();
